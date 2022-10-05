19ea045e7a3007297e7ac7e7bc0eb2732063724e
powerfullz <wleizc@hotmail.com>
2022-10-05 21:24:25 +0800

    Improve readability

--- batch_rename.py
+++ batch_rename.py
@@ -8,13 +8,10 @@ rows_num = table.nrows  # 获取表格的行数
 for i in range(1,rows_num):
     row_data = table.row_values(i)
     date = '-' + row_data[5].replace('年','.').replace('月','') # -2022.10
-    rename_dst = str(i) + ' ' + row_data[8].replace('\n', '') +' '+ row_data[1].replace('\n', '') + ' '+ row_data[4].replace('\n', '') + date + '.' + row_data[13].split(".")[-1]
+    names = row_data[8].replace('\n','')    # 姓名
+    prize_name = row_data[1].replace('\n', '')  # 奖项
+    prize_level = row_data[4].replace('\n', '') # 获奖等级
+    ext_name = '.' + row_data[13].split(".")[-1]    # 扩展文件名
+    rename_dst = str(i) + ' ' + names +' '+ prize_name +' '+ prize_level + date + ext_name
     print(rename_dst)   # 仅调试用
     os.rename(row_data[13], rename_dst)
-    
-
-
-'''
-names_src = os.listdir(path)
-os.rename(names_src[0],"电气工程" + "." + names_src[0].split(".")[1])
-'''
